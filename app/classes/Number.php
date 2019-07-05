df72580fd7203bcbf20138b6e5099ac4cb81eab4
Jordi <jmdesarrollo82@gmail.com>
2019-07-05 09:06:39 +0200

    Añadido signo de concatenación para no sobreescribir la variable declarada anteriormente

--- app/classes/Number.php
+++ app/classes/Number.php
@@ -18,7 +18,7 @@ class Number
         $output = '';
 
         if ($int % 3 === 0 || preg_match('/[3]/', $int) === 1) {
-            $output = 'Fizz';
+            $output .= 'Fizz';
         }
 
         if ($int % 5 === 0 || preg_match('/[5]/', $int) === 1) {
