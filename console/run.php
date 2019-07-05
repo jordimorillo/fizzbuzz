6f83a30616991eaf94a5a88778848121b3fd0564
Jordi <jmdesarrollo82@gmail.com>
2019-07-05 11:02:09 +0200

    Modificados los metodos estáticos por instancias

--- console/run.php
+++ console/run.php
@@ -2,6 +2,10 @@
 
 require('vendor/autoload.php');
 
+use Classes\Number;
 use Classes\Controller;
 
-echo Controller::makeFizzBuzz(1, 100);
+$Controller = new Controller;
+$Number = new Number;
+
+echo $Controller->makeFizzBuzz(1, 100, $Number);
